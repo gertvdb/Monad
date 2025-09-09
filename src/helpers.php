01e8df98c7aeb5434258e5a6c2419d04f2ad0295
Gert van den Buijs <gertvandenbuijs@hotmail.com>
2025-09-09 16:38:23 +0200

    Update helpers.php

--- src/helpers.php
+++ src/helpers.php
@@ -4,11 +4,8 @@ declare(strict_types=1);
 
 namespace Gertvdb\Monad;
 
-use InvalidArgumentException;
-use RuntimeException;
 use Gertvdb\Monad\Monads\Either\Failure;
 use Gertvdb\Monad\Monads\Either\Success;
-use Throwable;
 
 /**
  * Check if the given Either is a Success.
@@ -30,110 +27,4 @@ function isSuccess(Either $either): bool
 function isFailure(Either $either): bool
 {
     return $either instanceof Failure;
-}
-
-/**
- * @template TSuccess
- * @template TDefault
- * @param Either<TSuccess, mixed> $either
- * @param TDefault $default
- * @return TSuccess|TDefault
- */
-function getOrElse(Either $either, mixed $default): mixed
-{
-    return isSuccess($either) ? $either->unwrap() : $default;
-}
-
-function unwrapAs(Either $either, string $expectedClass): object
-{
-    if (isFailure($either)) {
-        throw new RuntimeException("Cannot unwrap failure");
-    }
-
-    $value = $either->unwrap();
-
-    if (!($value instanceof $expectedClass)) {
-        throw new InvalidArgumentException("Expected instance of $expectedClass, got " . get_debug_type($value));
-    }
-
-    return $value;
-}
-
-/**
- * @template TSuccess
- * @template TFailure of Throwable
- * @template TResult
- * @param Either<TSuccess, TFailure> $either
- * @param callable(TFailure): TResult $onFailure
- * @param callable(TSuccess): TResult $onSuccess
- * @return TResult
- */
-function fold(Either $either, callable $onFailure, callable $onSuccess): mixed
-{
-    return isSuccess($either)
-        ? $onSuccess($either->unwrap())
-        : $onFailure($either->unwrapError());
-}
-
-/**
- * @template TSuccess
- * @template TFailure of Throwable
- * @template TResult
- * @param Either<TSuccess, TFailure> $either
- * @param callable(TFailure): TResult $onFailure
- * @param callable(TSuccess): TResult $onSuccess
- * @return TResult
- */
-function doMatch(Either $either, callable $onSuccess, callable $onFailure): mixed
-{
-    return fold($either, $onFailure, $onSuccess);
-}
-
-/**
- * @template TSuccess
- * @template TFailure of Throwable
- * @param Either<TSuccess, TFailure> $either
- * @param mixed $value
- * @return bool
- */
-function contains(Either $either, mixed $value): bool
-{
-    return isSuccess($either) && $either->unwrap() === $value;
-}
-
-function recover(Either $either, callable $fn): Either
-{
-    if (isSuccess($either)) {
-        return $either;
-    }
-
-    return $either->lift($fn($either->unwrapError()));
-}
-
-function ensure(Either $either, callable $predicate, Fault $fault): Either
-{
-    if (isFailure($either)) {
-        return $either;
-    }
-
-    return $predicate($either->unwrap())
-        ? $either
-        : $either->fail($fault);
-}
-
-function sequence(array $eithers): Either
-{
-    $results = [];
-    $base = null;
-
-    foreach ($eithers as $either) {
-        if (isFailure($either)) {
-            return $either;
-        }
-
-        $base ??= $either;
-        $results[] = $either->unwrap();
-    }
-
-    return $base->lift($results);
 }
